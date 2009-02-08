8be059ec25ff1604335afcaf5cd9d6b19389895c
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@gmail.com>
2009-02-08 11:19:16 -0700

    Added basic wiki models

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from iridium.project.models import Project
 
 # Create your models here.
+class WikiPage(models.Model):
+    project = models.ForeignKey(Project)
+    name = models.CharField(max_length=120)
+    title = models.CharField(max_length=255)
+    published = models.DateField()
+    
+class WikiRevision(models.Model):
+    page = models.ForeignKey(WikiPage)
+    contents = models.TextField()
+    published = models.DateField()
