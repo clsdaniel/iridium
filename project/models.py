8a07becf570b970035e3d196b7f657f2b61714d1
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@gmail.com>
2009-02-08 11:00:25 -0700

    Added basic Project model

--- project/models.py
+++ project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=120)
+    desc = models.TextField()
+    admins = models.ManyToManyField(User)
+    devels = models.ManyToManyField(User)
+    
