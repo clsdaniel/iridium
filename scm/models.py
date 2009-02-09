29ac1dcf02bbd2789c3900644a34494dc40ae301
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@gmail.com>
2009-02-09 07:42:35 -0700

    Added basic repository model

--- scm/models.py
+++ scm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from iridium.project.models import Project
 
 # Create your models here.
+class Repository:
+    project = models.ForeignKey(Project)
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    path = models.CharField(max_length=255)
+    owner = models.ForeignKey(User)
+    
