a6dd530f7126ba365b1aac1faf6bfd8e0e69a7ad
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@gmail.com>
2009-02-09 08:24:39 -0700

    Updated scm urls

--- scm/urls.py
+++ scm/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'(?P<pid>[\d\w]*)/(?P<wid>[\d\w]*)', 'iridium.scm.views.viewRepo'),
+    (r'(?P<pid>[\d\w]*)/(?P<rid>[\d\w]*)/diff/(?P<cid>[\d\w]*)', 'iridium.scm.views.viewDiff'),
+    (r'(?P<pid>[\d\w]*)/(?P<rid>[\d\w]*)', 'iridium.scm.views.viewRepo'),
     (r'(?P<pid>[\d\w]*)', 'iridium.scm.views.listRepos'),
 )
