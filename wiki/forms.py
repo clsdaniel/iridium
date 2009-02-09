c05da0679f89d3d88151bfcd4adfefb583efe0bc
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@gmail.com>
2009-02-09 02:42:28 -0700

    Added forms to wiki

---
+++ wiki/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+class NewPageForm(forms.Form):
+    name = forms.CharField(label=_("Page name"), max_length=120)
+    
+class EditPageForm(forms.Form):
+    title = forms.CharField(label=_("Title"), max_length=120)
+    contents = forms.CharField(label=_("Content"), widget=forms.Textarea, max_length=120)
