f9a7439f9fc5e912a136af449da8716be83ec80f
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@gmail.com>
2009-02-17 18:32:59 -0700

    Added issue module models

--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from iridium.project.models import Project
 
 # Create your models here.
+class Issue(models.Model):
+    project = models.ForeignKey(Project)
+    title = models.CharField(max_length=255)
+    contents = models.TextField()
+    author = models.ForeignKey(User)
+    published = models.DateField()
+    assigned = models.ForeignKey(User)
+    priority = models.IntegerField()
+    status = models.IntegerField()
+    
+class IssueFile(models.Model):
+    issue = models.ForeignKey(Issue)
+    filename = models.CharField(max_length=255)
+    filedesc = models.CharField(max_length=255)
+    filesize = models.IntegerField()
+    filetype = models.CharField(max_length=32)
+    filehash = models.CharField(max_length=32)
+    
+class IssueComment(models.Model):
+    title = models.CharField(max_length=255)
+    contents = models.TextField()
+    author = models.ForeignKey(User)
+    published = models.DateField()
+    
