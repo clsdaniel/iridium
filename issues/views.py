35ef5c8336a5d33b1452f90376feb776883dc8ea
Carlos Daniel Ruvalcaba Valenzuela <clsdaniel@gmail.com>
2009-02-19 19:58:49 -0700

    [ISSUES] Added edit issue view

--- issues/views.py
+++ issues/views.py
@@ -21,6 +21,32 @@ from iridium.project.models import Project
 from models import *
 from forms import *
 
+issue_status = ((1, _("New")),
+                (2, _("Accepted")),
+                (3, _("Started")),
+                (4, _("Fixed")),
+                (5, _("Verified")),
+                (6, _("Invalid")),
+                (7, _("Duplicated")),
+                (8, _("WontFix")),
+                )
+
+issue_priority = ((1, _("Low")),
+                  (2, _("Medium")),
+                  (3, _("High")),
+                  (4, _("Critical")),
+                  (5, _("Blocker")),
+                  )
+
+issue_type = (("defect", _("Defect")),
+              ("enhancement", _("Enhancement")),
+              ("task", _("Task")),
+              )
+
+di_type = dict(issue_type)
+di_priority = dict(issue_priority)
+di_status = dict(issue_status)
+
 def getProject(pid):
     if pid.isdigit():
         proj = Project.objects.get(id=int(pid))
@@ -73,6 +99,47 @@ def newIssue(request, pid):
         return HttpResponseRedirect('/i/%s/%i' % (pid, issue.id))
         
     return dict(section='issues', pid=pid, title="New issue", form=form)
+
+@login_required
+@template("generic_form.html")
+def editIssue(request, pid, sid):
+    if request.method == "POST":
+        issue = Issue.objects.get(id=int(sid))
+        
+        autocomment = "<p>Changes:</p><ol>"
+        
+        if issue.issuetype != request.POST['issuetype']:
+            autocomment += "<li>Changed Issue Type: from %s to %s</li>" % (di_type[issue.issuetype], di_type[request.POST['issuetype']])
+            issue.issuetype = request.POST['issuetype']
+            
+        priority = int(request.POST['priority'])
+        if issue.priority != priority:
+            autocomment += "<li>Changed Issue Priority: from %s to %s</li>" % (di_priority[issue.priority], di_priority[priority])
+            issue.priority = priority
+   
+        status = int(request.POST['status'])
+        if issue.status != status:
+            autocomment += "<li>Changed Issue Status: from %s to %s</li>" % (di_status[issue.status], di_status[status])
+            issue.status = status
+            
+        autocomment += "</ol>"
+        autocomment += request.POST['comment']
+        
+        title = "ISSUE UPDATE"
+        comment = issue.comments.create(title=title, contents=autocomment,
+                                    author=request.user, issue=issue,
+                                    published=datetime.datetime.now())
+        comment.save()
+        issue.save()
+        
+        
+    issue = Issue.objects.get(id=int(sid))
+    title = "Edit: " + issue.title
+    
+    form = EditIssueForm(dict(issuetype=issue.issuetype, priority=issue.priority,
+                         status=issue.status, comment=""))
+    
+    return dict(section='issues', pid=pid, title=title, form=form)
 
 @login_required
 def commentIssue(request, pid, sid):
